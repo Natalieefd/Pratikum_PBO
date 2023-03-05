2cb2c0454ea8b6e8ddc31b821d47cce3ecf87c7b
Natalieefd <nataliefuad2003@gmail.com>
2023-03-06 00:22:04 +0800

    POSTTEST 1

---
+++ POSTTEST 1/src/POSTTEST 1/PBO/DataPeserta.java
@@ -0,0 +1,16 @@
+
+package PBO;
+
+public class DataPeserta {
+    String nama;
+    int angkatan;
+    String kategoriLomba;
+    String asalInstansi;
+    
+    public DataPeserta(String Nama, int Angkatan, String KategoriLomba, String AsalInstansi){
+        this.nama = Nama;
+        this.angkatan = Angkatan;
+        this.kategoriLomba = KategoriLomba;
+        this.asalInstansi = AsalInstansi;
+    }
+}
